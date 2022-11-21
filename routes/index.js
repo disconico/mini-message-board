da085b9f80b8ef5322174e83bf535a87a8b7f371
Nicolas Guerin <disconico@outlook.com>
2022-11-21 18:14:41 +0100

    First commit

---
+++ routes/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+];
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Mini Message Board',
+    messages,
+  });
+});
+
+router.get('/new', (req, res, next) => {
+  res.render('form', { title: 'New message' });
+});
+
+router.post('/new', (req, res, next) => {
+  const user = req.body.user;
+  const text = req.body.text;
+  messages.push({ text, user, added: new Date() });
+  res.redirect('/');
+});
+
+module.exports = router;
